096100409281e30402385a15e5350f32f5703b47
Simon Velasquez c <84815562+matryx-root@users.noreply.github.com>
2024-03-22 19:03:04 -0700

    listo

--- dist/Impuesto.js
+++ dist/Impuesto.js
@@ -1,30 +1,16 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
-function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 // Definición de la clase Impuesto
-var Impuesto = /*#__PURE__*/function () {
+class Impuesto {
   // Constructor que inicializa los atributos montoBrutoAnual y deducciones
-  function Impuesto(montoBrutoAnual, deducciones) {
-    _classCallCheck(this, Impuesto);
+  constructor(montoBrutoAnual, deducciones) {
     this._montoBrutoAnual = montoBrutoAnual;
     this._deducciones = deducciones;
   }
 
   // Método para calcular el impuesto a pagar
-  return _createClass(Impuesto, [{
-    key: "calcularImpuesto",
-    value: function calcularImpuesto() {
-      return (this._montoBrutoAnual - this._deducciones) * 0.21;
-    }
-  }]);
-}(); // Exporta la clase Impuesto para que pueda ser utilizada en otros archivos
-var _default = exports["default"] = Impuesto;
+  calcularImpuesto() {
+    return (this._montoBrutoAnual - this._deducciones) * 0.21;
+  }
+}
+
+// Exporta la clase Impuesto para que pueda ser utilizada en otros archivos
+export default Impuesto;
--- src/main.js
+++ src/main.js
@@ -4,20 +4,35 @@ import Cliente from './Cliente.js';
 // Importa la clase Impuesto desde el archivo Impuesto.js
 import Impuesto from './Impuesto.js';
 
-// Crea una nueva instancia de Cliente con los datos proporcionados
-const cliente1 = new Cliente('Juan Pérez', '12345678-9', 1000000, 500000);
+// Crea más instancias de Cliente con diferentes datos
+const cliente1 = new Cliente('Juan Pérez', '12345678-9', 1500000, 50000);
+const cliente2 = new Cliente('María García', '98765432-1', 800000, 200000);
+const cliente3 = new Cliente('Carlos López', '45678901-2', 2000000, 300000);
+const cliente4 = new Cliente('Ana Rodríguez', '13579024-3', 1200000, 100000);
+const cliente5 = new Cliente('Pedro Ramírez', '24680135-4', 900000, 80000);
 
-// Imprime en la consola el nombre del cliente
+// Imprime los datos y el impuesto a pagar para cada cliente
 console.log(`Nombre: ${cliente1.nombre}`);
-
-// Imprime en la consola el rut del cliente
 console.log(`Rut: ${cliente1.rut}`);
-
-// Calcula e imprime en la consola el impuesto a pagar por el cliente
 console.log(`Impuesto a pagar: $${cliente1.calcularImpuesto()}`);
+console.log(`Impuesto calculado: $${new Impuesto(cliente1._montoBrutoAnual, cliente1._deducciones).calcularImpuesto()}`);
 
-// Crea una nueva instancia de Impuesto con los datos proporcionados
-const impuesto1 = new Impuesto(1000000, 500000);
+console.log(`Nombre: ${cliente2.nombre}`);
+console.log(`Rut: ${cliente2.rut}`);
+console.log(`Impuesto a pagar: $${cliente2.calcularImpuesto()}`);
+console.log(`Impuesto calculado: $${new Impuesto(cliente2._montoBrutoAnual, cliente2._deducciones).calcularImpuesto()}`);
 
-// Calcula e imprime en la consola el impuesto calculado
-console.log(`Impuesto calculado: $${impuesto1.calcularImpuesto()}`);
+console.log(`Nombre: ${cliente3.nombre}`);
+console.log(`Rut: ${cliente3.rut}`);
+console.log(`Impuesto a pagar: $${cliente3.calcularImpuesto()}`);
+console.log(`Impuesto calculado: $${new Impuesto(cliente3._montoBrutoAnual, cliente3._deducciones).calcularImpuesto()}`);
+
+console.log(`Nombre: ${cliente4.nombre}`);
+console.log(`Rut: ${cliente4.rut}`);
+console.log(`Impuesto a pagar: $${cliente4.calcularImpuesto()}`);
+console.log(`Impuesto calculado: $${new Impuesto(cliente4._montoBrutoAnual, cliente4._deducciones).calcularImpuesto()}`);
+
+console.log(`Nombre: ${cliente5.nombre}`);
+console.log(`Rut: ${cliente5.rut}`);
+console.log(`Impuesto a pagar: $${cliente5.calcularImpuesto()}`);
+console.log(`Impuesto calculado: $${new Impuesto(cliente5._montoBrutoAnual, cliente5._deducciones).calcularImpuesto()}`);
